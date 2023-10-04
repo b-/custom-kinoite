37965e97086812c2abb827fb75e36c687fde45e9
bri <284789+b-@users.noreply.github.com>
2023-10-04 19:56:20 -0400

    add code-insiders

---
+++ scripts/vscode.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# from https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
+# RHEL, Fedora, and CentOS based distributions
+#
+# We currently ship the stable 64-bit VS Code in a yum repository, the following script will install the key and repository:
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# Then update the package cache and install the package using dnf (Fedora 22 and above):
+
+dnf check-update
+sudo dnf install code-insiders
